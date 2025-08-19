194bb3accb7d403dbf86f36599e4bb3e5807f122
Ludmila S. Leboso Sánchez <ludmilaleboso@hotmail.com>
2025-08-19 20:16:57 +0200

    PersonajesOK

--- ScriptEjercicio1.sql
+++ ScriptEjercicio1.sql
@@ -14,4 +14,11 @@ insert into personajesdecomic (nombre, especie) values
 ('Gamora Zen Whoberi Ben Titan', 'Zen-Whoberian'),
 ('Valeria Richards/von Doom', 'Mutant'),
 ('White Phoenix - Jean Grey', 'Primal Force/Mutant'),
-('Scarlet Witch - Wanda Maximoff', 'Mutant')
+('Scarlet Witch - Wanda Maximoff', 'Mutant');
+
+
+update personajesdecomic set frase_favorita = 'You break the rules and become a hero. I do it, I become the enemy. That doesn’t seem fair.' where id = 5;
+
+update personajesdecomic set frase_favorita = 'Jean is only the house where I live' where nombre = 'White Phoenix - Jean Grey';
+
+
