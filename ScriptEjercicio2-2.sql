7f31b4f3f5db633fdae14473ff0cd3375a571509
Ludmila S. Leboso Sánchez <ludmilaleboso@hotmail.com>
2025-08-20 20:11:26 +0200

    TodoOk?

---
+++ ScriptEjercicio2-2.sql
@@ -0,0 +1,17 @@
+create table paises (
+id int auto_increment primary key,
+
+country varchar (50),
+capital varchar(50),
+pib int
+
+)
+
+insert into paises (country, capital, pib) values
+('Éire', 'Baile Átha Cliath', 106059),
+('Montserrat', 'Plymouth', 16199),
+('Tuvalu', 'Funafuti', 6001),
+('Islas Marshall', 'Majuro', 6711),
+('Palestina', 'Jersualen', 3396);
+
+
